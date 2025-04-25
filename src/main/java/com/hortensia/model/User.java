075254f9370aa570c56fd7b559df2bdc7df85a19
dancastros <40213363+dancastros@users.noreply.github.com>
2025-04-24 22:49:55 -0600

    Add src/main/java/com/hortensia/model/User.java 1745556595123

---
+++ src/main/java/com/hortensia/model/User.java
@@ -0,0 +1,35 @@
+package com.hortensia.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, unique = true)
+    private String dni;
+    
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String phone;
+    
+    @Column(nullable = false)
+    private String address;
+    
+    @Column(nullable = false)
+    private LocalDate birthday;
+}
