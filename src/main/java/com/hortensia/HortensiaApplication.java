626147b0fbb278aa1eda0139dafc08e1e7173c19
dancastros <40213363+dancastros@users.noreply.github.com>
2025-04-24 22:49:57 -0600

    Add src/main/java/com/hortensia/HortensiaApplication.java 1745556597217

---
+++ src/main/java/com/hortensia/HortensiaApplication.java
@@ -0,0 +1,11 @@
+package com.hortensia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HortensiaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HortensiaApplication.class, args);
+    }
+}
